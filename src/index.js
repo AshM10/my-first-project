5ac8a7ac17d608b691a73abffa494eda6a8688ee
Asther Marie Moreno <ashterfuentes@gmail.com>
2022-01-22 14:18:12 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "0a521eaf234a3a56f45252fac3c737ad";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
